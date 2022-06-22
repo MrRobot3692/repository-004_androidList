ad2200454eb399af6cae4e97add4892f083dc399
MrRobot3692 <98954434+MrRobot3692@users.noreply.github.com>
2022-06-22 14:29:27 +0300

    Created listView

--- app/src/main/java/com/example/list/MainActivity.java
+++ app/src/main/java/com/example/list/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.list;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    String[] listItems = new String[20];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listView);
+        for(int i = 0; i<20; i++){
+            listItems[i] = "list item " + (i+1);
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, listItems);
+        listView.setAdapter(adapter);
     }
 }
